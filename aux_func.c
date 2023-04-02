17c8094a7562ff1a01f700476ffdd1fed4ebe942
Mariana Echeto <echetomariana@gmail.com>
2023-04-02 04:36:18 -0300

    MIN_INT

--- aux_func.c
+++ aux_func.c
@@ -1,4 +1,26 @@
 #include "main.h"
+#include <limits.h>
+
+/**
+ * print_int_min - print integer recursively
+ * Return: len.
+ */
+int print_int_min(void)
+{
+	_putchar('-');
+	_putchar('2');
+	_putchar('1');
+	_putchar('4');
+	_putchar('7');
+	_putchar('4');
+	_putchar('8');
+	_putchar('3');
+	_putchar('6');
+	_putchar('4');
+	_putchar('8');
+	return (11);
+}
+
 /**
  * printi_rec - print integer recursively
  * @n: input int
@@ -7,6 +29,9 @@
 int printi_rec(int n)
 {
 	int r;
+
+	if (n == INT_MIN)
+		return (print_int_min());
 
 	r = 0;
 	if (n < 0)
